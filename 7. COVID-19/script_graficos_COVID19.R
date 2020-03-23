a808c4fc7364701f6877dded3d1dddf51f7ee1a8
jcsd84 <jcsd84.jc@gmail.com>
2020-03-23 16:35:57 -0500

    Update graficas COVID-19

--- 7. COVID-19/script_graficos_COVID19.R
+++ 7. COVID-19/script_graficos_COVID19.R
@@ -19,24 +19,35 @@ head(data.frame(data_COVID))
 str(data_COVID)
 
 data_COVID$DIA <- dmy(data_COVID$DIA) #formatear fecha
+### Creacion de casos en Lima
+data_COVID$Positivos_Lima <- cumsum(data_COVID$Positivos_Lima_Diarios)
 
 ### Creacion de casos en provincia
 data_COVID$Positivos_Provincias <- data_COVID$NUMERO_CASOS - data_COVID$Positivos_Lima
 
 ### Creacion casos por dia 
 
-data_COVID$CASOS_diarios <- NA # Linea vacia
+data_COVID$CASOS_diarios <- data_COVID$Positivos_Lima_Diarios + data_COVID$Positivos_Provincia_diarios
 
-data_COVID$CASOS_diarios[1] <- data_COVID$NUMERO_CASOS[1] # Primera linea
-casos <- data_COVID$NUMERO_CASOS[2:nrow(data_COVID)] # Vector de la segunda hacia abajo
-casos <- c(casos, 0) # Agregar un 0 al final para tener misma longitud
-dif_casos <- casos - data_COVID$NUMERO_CASOS # Calcular resta
-dif_casos <- dif_casos[-length(dif_casos)] # Sacar el ultimo valor
-data_COVID$CASOS_diarios[2:nrow(data_COVID)] <-dif_casos # Poner los valores en la columna vacía
+### Creacion de testeados
 
-### Creacion proporcion testeados positivos diarios
+data_COVID$Testeados <- data_COVID$NUMERO_CASOS + data_COVID$Descartados
 
-data_COVID$Prop_Indidencia_diaria <- (data_COVID$CASOS_diarios/data_COVID$Testeados)*100
+### Creacion proporcion testeados positivos 
+
+data_COVID$Prop_Indidencia_diaria <- (data_COVID$NUMERO_CASOS/data_COVID$Testeados)*100
+
+### Creacion proporcion diaria (por hacer)
+
+# data_COVID$Prop_Indidencia_diaria <- NA # Linea vacia
+# 
+# data_COVID$CASOS_diarios[1] <- data_COVID$NUMERO_CASOS[1] # Primera linea
+# casos <- data_COVID$NUMERO_CASOS[2:nrow(data_COVID)] # Vector de la segunda hacia abajo
+# casos <- c(casos, 0) # Agregar un 0 al final para tener misma longitud
+# dif_casos <- casos - data_COVID$NUMERO_CASOS # Calcular resta
+# dif_casos <- dif_casos[-length(dif_casos)] # Sacar el ultimo valor
+# data_COVID$CASOS_diarios[2:nrow(data_COVID)] <-dif_casos # Poner los valores en la columna vacía
+# 
     
   
 
@@ -84,21 +95,6 @@ C_Provincia <- ggplot(data_COVID, aes(x=DIA, y =Positivos_Provincias))+
   scale_y_continuous(breaks = seq(0, 100, by = 20), 
                      limits = c(0,100))
 
-#### Cantidad de tests por día
-
-C_Tests_diarios <- ggplot(data_COVID, aes(x=DIA, y =Testeados))+
-  geom_line(color = "lightblue")+
-  geom_point(color = "blue")+
-  geom_smooth(method = "loess", formula = 'y ~ x')+
-  theme(panel.background = element_blank(),
-        axis.text.x = element_text(angle = 90, hjust = 1))+
-  xlab(" ")+
-  ylab("Tests realizados acumulados COVID-19")+
-  scale_x_date(breaks='1 day', date_labels = "%e-%m-%Y")+
-  scale_y_continuous(breaks = seq(0, 1200, by = 50), 
-                     limits = c(0,1200))
-
-
 #### Crecimiento diario
 
 Crec_diario <- ggplot(data_COVID, aes(x=DIA, y =CASOS_diarios))+
@@ -112,6 +108,23 @@ Crec_diario <- ggplot(data_COVID, aes(x=DIA, y =CASOS_diarios))+
   scale_x_date(breaks='1 day', date_labels = "%e-%m-%Y")+
   scale_y_continuous(breaks = seq(0, 100, by = 20), 
                      limits = c(0,100))
+
+#### Cantidad de tests aculumulado
+
+C_Tests_acumulado <- ggplot(data_COVID, aes(x=DIA, y =Testeados))+
+  geom_line(color = "lightblue")+
+  geom_point(color = "blue")+
+  geom_smooth(method = "loess", formula = 'y ~ x')+
+  theme(panel.background = element_blank(),
+        axis.text.x = element_text(angle = 90, hjust = 1))+
+  xlab(" ")+
+  ylab("Tests realizados acumulados COVID-19")+
+  scale_x_date(breaks='1 day', date_labels = "%e-%m-%Y")+
+  scale_y_continuous(breaks = seq(0, 7000, by = 400), 
+                     limits = c(0,7000))
+
+
+
 
 #### Proporción de positivos con respecto a casos testeados por día
 
