dadd475b8b47d856f9bb194da9ab8d8ac86681b8
jcsd84 <jsaraviadrago@gmail.com>
2020-08-12 00:21:02 -0500

    Update

--- 7. World Cup stats (2020)/Script_WC_NW.R
+++ 7. World Cup stats (2020)/Script_WC_NW.R
@@ -7,7 +7,7 @@ library(igraph)
 library(ggraph)
 library(dplyr)
 library(tidygraph)
-
+library(viridis)
 
 
 WC <- fread("/Users/home/PycharmProjects/Portafolio/7. World Cup stats (2020)/WC_matches_final.csv")
@@ -30,28 +30,21 @@ set.seed(1111)
 figure <- ggraph(WC.graph, layout = 'mds') + 
   geom_edge_fan(color = "grey")+
   geom_node_point(aes(colour = betweenness)) +
-  geom_node_text(aes(label = dplyr::if_else(betweenness > 350, as.character(name), NA_character_)),
-                 size = 2, repel = T)+
+  geom_node_text(aes(label = dplyr::if_else(betweenness > 350,
+                                            as.character(name), NA_character_)),
+                 size = 3, repel = T)+
 theme_graph() +
-  theme(legend.title=element_blank())
+  theme(legend.title=element_blank()) +
+  scale_color_viridis()
 
-ggraph(WC.graph, layout = 'kk') + 
-  geom_edge_density() + 
-  geom_edge_link(alpha = 0.25)
+
 
 #ggsave("WC_NETWORK_DATA.png", plot =figure,
 #       width = 10, height = 10, 
 #       limitsize = F)
 
-##### Making clusters ###
-
-#WC.graph %>% 
-#mutate(community = as.factor(group_infomap())) %>% 
-#ggraph(layout = 'mds') + 
-#geom_edge_link(aes(alpha = ..index..), show.legend = FALSE) + 
-#geom_node_point(aes(colour = community), size = 3) + 
-#theme_graph()
 
 
-#c('star', 'circle', 'gem', 'dh', 'graphopt', 'grid', 'mds', 
-#  'randomly', 'fr', 'kk', 'drl', 'lgl')
+
+
+
