25ae1633fb5c73c99ef45151083586befd4f48ab
jcsd84 <jsaraviadrago@gmail.com>
2021-02-19 18:18:48 -0500

    Update

--- 0. Fun 3D plots and art/Scripts_triforce.R
+++ 0. Fun 3D plots and art/Scripts_triforce.R
@@ -7,7 +7,7 @@ library(sf)
 #plot(x,y)
 
 # Creating the triforce
-multipoint <-  st_multipoint(matrix(c(0,1,2,3,1,2,4,0,3,0,3,3,6,0),
+multipoint <-  st_multipoint(matrix(c(0,10,20,30,10,20,40,0,30,0,30,30,60,0),
                                     ncol = 2))
 
 # Creating the polygon
