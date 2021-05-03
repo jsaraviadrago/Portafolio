a26226ea58c5d60acefdf41e6bde624495d6a49f
jcsd84 <jsaraviadrago@gmail.com>
2021-05-03 16:27:14 -0500

    Update

---
+++ 11. Crypto/Script_Cryptos.R
@@ -0,0 +1,10 @@
+# Script for crypto analysis
+
+library(data.table) #open dataframes
+library(dplyr) # manipulate dataframes
+
+data_bitcoin <- fread("/Users/home/Dropbox/Databases/Historical_crypto/coin_Bitcoin.csv")
+data_etherum <- fread("/Users/home/Dropbox/Databases/Historical_crypto/coin_Ethereum.csv")
+
+
+
