6df1553df4538e4e114f6ade811ace3cc4b2540b
jcsd84 <jsaraviadrago@gmail.com>
2021-07-02 15:47:25 -0500

    Updating file

--- 7. World Cup stats (2020)/Ebook/Script_analysis.py
+++ 7. World Cup stats (2020)/Ebook/Script_analysis.py
@@ -2,23 +2,23 @@ import pandas as pd
 import sqlite3 as sq3
 
 conn = sq3.connect('World_cup.sqlite')
-cursor = conn.cursor()
+cur = conn.cursor()
 
 cur.execute('DROP TABLE IF EXISTS WC')
 
 cur.execute('CREATE TABLE WC (Match int, Year int,'
-            'Teams = nvarchar(50), Goals int, '
-            'Rival = nvarchar(50) ')
+            'Teams nvarchar(50), Goals int, '
+            'Rival nvarchar(50))')
 
 data_WC = pd.read_csv("/Users/home/Documents/MP blog 2021/Data/World Cup/data_final_WC_matches_long.csv")
 
-data_WC.to_sql('data_WC', conn, if_exists = replace, index = True)
+data_WC.to_sql('WC', conn, if_exists='replace', index = True)
 
-cur.execute('SELECT Teams, Goals, FROM WC')
+cur.execute('SELECT Year, AVG(Goals) FROM WC GROUP BY Year')
 
-for row in cur.fetchall:
+
+for row in cur.fetchall():
     print(row)
 
 conn.commit()
-conn.close
-
+conn.close()
